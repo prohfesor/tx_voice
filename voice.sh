d8d97d5bfed16808bdaa5678cd7f9da181e3ffd9
Pavel Dvoinos <pavel.dvoinos@transferwise.com>
2017-09-14 22:50:25 +0300

    options and truncating filename

--- voice.sh
+++ voice.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-LANG=de
+LANG=en
+PLAY_SOUND=false
+LOWERCASE=false
+TRUNCATE_LONG=true
 
 while read LINE; do
  FILENAME=${LINE//[^a-zA-Z0-9]/_}
- FILENAME="${FILENAME,,}"
+ if $LOWERCASE ; then
+  FILENAME="${FILENAME,,}"
+ fi
  echo "$LINE -> $FILENAME"
  gtts-cli "$LINE" -o $FILENAME.mp3 -l $LANG
- #mpg123 $FILENAME.mp3
+ if $PLAY_SOUND ; then
+  mpg123 $FILENAME.mp3
+ fi
 done < "voice_list.txt"
 
 for FILEMP3 in *.mp3
@@ -15,4 +22,21 @@ do
  #need wav microsoft pcm 16 bit mono
  ffmpeg -loglevel warning -y -i $FILEMP3 -filter:a "volume=5" -acodec pcm_s16le -ar 22050 -ac 1 ${FILEMP3%.*}.wav
  rm -f $FILEMP3
+done
+
+for FILEWAV in *.wav
+do
+ if [ ${#FILEWAV} -gt 12 ] && $TRUNCATE_LONG ; then
+  echo "$FILEWAV - too long, trying to remove underscores"
+  FILESHORTER=${FILEWAV//_/}
+  if [ ${#FILESHORTER} -gt 12 ] ; then
+   echo " $FILESHORTER - still too long, will truncate"
+   FILESHORTEST="${FILESHORTER:0:8}.wav"
+   echo "  $FILESHORTEST"
+   mv $FILEWAV $FILESHORTEST
+  else
+   mv $FILEWAV $FILESHORTER
+   echo "  $FILESHORTER"
+  fi
+ fi
 done
